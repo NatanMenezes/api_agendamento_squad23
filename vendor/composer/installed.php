d98c43ccf2edf0d3f171482cd462f99c080b6b98
Natan Menezes <natanmenezes31@gmail.com>
2021-09-04 17:27:14 -0300

    criado o projeto e adicionadas as dependencias

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'squad23/agendamento',
+  ),
+  'versions' => 
+  array (
+    'coffeecode/datalayer' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '928012861d39b855c91485a0f1bd29d202c2fd08',
+    ),
+    'squad23/agendamento' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
