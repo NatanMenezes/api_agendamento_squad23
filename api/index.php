11bc6655bb3dabe85166db326f47b5e681f896e4
Natan Menezes <natanmenezes31@gmail.com>
2021-09-06 19:44:46 -0300

    Criada a funcionalidade de login na API

---
+++ api/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require "../vendor/autoload.php";
+
+use Source\CRUD\Consultores;
+
+var_dump(Consultores::login("natanmenezes31@gmail.com", "AdMiNdOsIsTeMa"));
---
+++ api/login/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require "../../vendor/autoload.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+use Source\CRUD\Consultores;
+
+if(isset($_POST['email']) && isset($_POST['senha']))
+{
+    $verifica = Consultores::login($_POST['email'], $_POST['senha']);
+
+    if(!$verifica)
+    {
+        echo json_encode(["status"=>"invalido"]);
+    } else 
+    {
+        echo json_encode(["status"=>"sucesso"]);
+    }
+};
