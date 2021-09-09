b121c40eca4182ed64d2b1754ee5d48107012caf
Natan Menezes <natanmenezes31@gmail.com>
2021-09-09 13:56:50 -0300

    Formatação do retorno de datas e turnos

--- api/agendamentos/index.php
+++ api/agendamentos/index.php
@@ -4,10 +4,12 @@ require_once( "../../vendor/autoload.php");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 use Source\Controller\Agendamento;
+use Source\Controller\Helpers;
 
 if(isset($_POST["id_funcionario"]))
 {
     $res = Agendamento::findByIdFuncionario($_POST["id_funcionario"]);
+    $res = Helpers::separarDataTurno($res);
     echo json_encode(["status"=>true,"data"=>$res, "message"=>"Agendamentos enviados com sucesso"], JSON_UNESCAPED_SLASHES);
 }else{
     echo json_encode(["status"=>false,"data"=>$res, "message"=>"Dados insuficientes"]);
--- source/Controller/Helpers.php
+++ source/Controller/Helpers.php
@@ -1,11 +1,24 @@
 <?php
 
 namespace Source\Controller;
+use Source\Controller\Agendamento;
 
 class Helpers
 {
     static function juntarDataTurno ($data, $turno)
     {
         return "$data $turno";
+    }
+
+    static function separarDataTurno ($arr)
+    {
+        $res = [];
+        foreach($arr as $el)
+        {
+            $el["turno"] = substr($el["data"], 11, 1);
+            $el["data"] = substr($el['data'], 0, 10);
+            array_push($res, $el);
+        }
+        return $res;
     }
 }
