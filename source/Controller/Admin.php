afb9b76a466c503672b3f8cd866bcde84903c7a7
Natan Menezes <natanmenezes31@gmail.com>
2021-09-09 16:20:19 -0300

    Ver e modificar a capacidade das estações de trabalho

--- source/Controller/Admin.php
+++ source/Controller/Admin.php
@@ -24,4 +24,34 @@ class Admin
         $stmt->execute();
         return $stmt->fetchAll(\PDO::FETCH_ASSOC)[0];
     }
+
+    static function mudarCapacidade ($n, $estacao)
+    {
+        $sql = "UPDATE config SET valor=? WHERE campo=?";
+        $stmt = Agendamentos::getConn()->prepare($sql);
+        $stmt->bindValue(1, $n);
+        $stmt->bindValue(2, "capacidade_$estacao");
+        $stmt->execute();
+        return Admin::verCapacidade();
+    }
+
+    static function verCapacidade ($estacao = "x")
+    {
+        if($estacao == "x"){
+            $sql = "SELECT * FROM config WHERE campo=?";
+            $stmt = Agendamentos::getConn()->prepare($sql);
+            $stmt->bindValue(1, "capacidade_SP");
+            $stmt->execute();
+            $sp = $stmt->fetchAll(\PDO::FETCH_ASSOC)[0];
+            $stmt->bindValue(1, "capacidade_Santos");
+            $stmt->execute();
+            $santos = $stmt->fetchAll(\PDO::FETCH_ASSOC)[0];
+            return ["SP"=>$sp['valor'], "Santos"=>$santos['valor']];
+        }
+        $sql = "SELECT * FROM config WHERE campo=?";
+        $stmt = Agendamentos::getConn()->prepare($sql);
+        $stmt->bindValue(1, "capacidade_$estacao");
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC)[0]; 
+    }
 }
