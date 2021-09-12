abb029df5b4e88ed35c68d8f31315f4b8131c234
Natan Menezes <natanmenezes31@gmail.com>
2021-09-11 21:18:40 -0300

    Controlando o retorno de agendamentos já passados

--- api/agendamentos/index.php
+++ api/agendamentos/index.php
@@ -10,6 +10,7 @@ if(isset($_POST["id_funcionario"]))
 {
     $res = Agendamento::findByIdFuncionario($_POST["id_funcionario"]);
     $res = Helpers::separarDataTurno($res);
+    $res = Helpers::agendamentosPassados($res);
     echo json_encode(["status"=>true,"data"=>$res, "message"=>"Agendamentos enviados com sucesso"], JSON_UNESCAPED_SLASHES);
 }else{
     echo json_encode(["status"=>false,"data"=>$res, "message"=>"Dados insuficientes"]);
