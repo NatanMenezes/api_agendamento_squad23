daa3626bcaf6cf2e71faf7218adf12f922ce8d20
Matheus Honorato <matheuswebmw@gmail.com>
2021-09-15 10:11:13 -0300

    Correção erro CORS no endpoint e arredondamento número

--- api/Calendario/Filtro/index.php
+++ api/Calendario/Filtro/index.php
@@ -1,8 +1,12 @@
 <?php
 
 require_once("../../../vendor/autoload.php");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 
 use Source\Controller\Calendario;
 use Source\Controller\Helpers;
@@ -21,19 +25,18 @@ if (isset($_POST["data"]) && isset($_POST["estacao"])) {
     $x = (($capacidade * $regulamento) / 100);
     if (!$tarde == null) {
         foreach ($tarde as $value) {
-            $totalt = $totalt + 1;
+            $totalt = $totalt + 1; //
         }
     }
     if (!$manha == null) {
         foreach ($manha as $value) {
-            $totalm = $totalm + 1;
+            $totalm = $totalm + 1; //
         }
     }
-    $capacidadem = round((100 * $totalm) / $x, 2) / 100;
-    $capacidadet = round((100 * $totalt) / $x, 2) / 100;
+    $capacidadem = (((100 * $totalm) / $x) / 100) / 100;
+    $capacidadet = (((100 * $totalt) / $x) / 100) / 100;
     $capacidadeambos = $capacidadem + $capacidadet;
-
-    echo json_encode(["Data" => $data, "Estacao" => $estacao, "Agendados_Manha" => $totalm, "Capacidade_Usada_Manha" => $capacidadem, "Agendados_Tarde" => $totalt, "Capacidade_Usada_Tarde" => $capacidadet, "Capacidade_Ambos" => $capacidadeambos], JSON_UNESCAPED_SLASHES);
+    echo json_encode(["Data" => $data, "Estacao" => $estacao, "Agendados_Manha" => $totalm, "Capacidade_Usada_Manha" => number_format($capacidadem, 4, '.', ''), "Agendados_Tarde" => $totalt, "Capacidade_Usada_Tarde" => number_format($capacidadet, 4, '.', ''), "Capacidade_Ambos" => number_format($capacidadeambos, 4, '.', '')], JSON_UNESCAPED_SLASHES);
 } else {
     echo json_encode(["message" => "Dados insuficientes"]);
 };
--- api/Calendario/index.php
+++ api/Calendario/index.php
@@ -1,8 +1,12 @@
 <?php
 
 require_once("../../vendor/autoload.php");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 
 use Source\Controller\Calendario;
 
